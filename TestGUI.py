d3229e4ea27cce9fffd867a052df60790c08cf8a
nathan <nathan.e.mcguire@gmail.com>
2021-08-20 17:35:58 -0400

    tweaked TestGUI.py

--- TestGUI.py
+++ TestGUI.py
@@ -11,6 +11,7 @@ def callback(data):
     """You can pass callbacks to the GUI now, that can be triggered from buttons"""
     print(data)
 
+
 if __name__ == '__main__':
     # GUI = RobotGUI2("config/GUIConfig.xml")
     GUI = RobotGUI2("config/BasicConfig.xml", testMode=True)
@@ -38,7 +39,8 @@ if __name__ == '__main__':
         if j > 360:
             j = 0
 
-        dataPassDict = {"test": "{}".format(random.random()), "batteryVoltage": "{}".format(random.random()), "current": str(random.random() * 100), "spinny": i}
+        dataPassDict = {"test": "{}".format(random.random()), "batteryVoltage": "{}".format(random.random()),
+                        "current": str(random.random() * 100), "spinny": i}
 
         if video:
             try:
@@ -74,10 +76,13 @@ if __name__ == '__main__':
         else:
             dataPassDict["armed"] = True
 
-        testDict = {"aaa": [["hi", "aaa"], ["bbb", random.random()]], "bbb": [["aa", "  {}".format(random.random())], ["bbb", random.random()], ["c", random.random()], ["ddddddddd", random.random()]]}
+        testDict = {"aaa": [["hi", "aaa"], ["bbb", random.random()]],
+                    "bbb": [["aa", "  {}".format(random.random())], ["bbb", random.random()], ["c", random.random()],
+                            ["ddddddddd", random.random()]]}
         dataPassDict["diagnostics_agg"] = testDict
 
-        annunciator = [["Overall", 0, "Test"], ["Battery", 1, "Test2"], ["Lights", 2, "Test3"], ["aaaaaaaaaaaaaaaaaaaaaaaaaa", 0, "test4"]]
+        annunciator = [["Overall", 0, "Test"], ["Battery", 1, "Test2"], ["Lights", 2, "Test3"],
+                       ["aaaaaaaaaaaaaaaaaaaaaaaaaa", 0, "test4"]]
         dataPassDict["annunciator"] = annunciator
 
         annunciator2 = [["aaa", 2, "Test"], ["bbb", 0, "Test2"], ["ccc", 0, "Test3"], ["ddddd", 3, "test4"]]
